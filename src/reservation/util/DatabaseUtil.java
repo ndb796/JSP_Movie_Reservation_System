c57ab7dff2620f80cb891b3dbd0079dc1d561b65
나동빈 <@LAPTOP-RLP93JS3>
2018-05-22 11:22:56 +0900

    서버 세팅 기초 작업

---
+++ src/reservation/util/DatabaseUtil.java
@@ -0,0 +1,27 @@
+package reservation.util;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DatabaseUtil {
+
+	public static Connection getConnection() {
+		Connection conn = null;
+		try {
+			// 텍스트 파일에는 한 줄씩 데이터베이스 아이디, 비밀번호를 입력하여 관리합니다.
+	        BufferedReader in = new BufferedReader(new FileReader(ServerUtil.authenticationPath + "Database.txt"));
+			String dbURL = "jdbc:mysql://localhost:3306/" + in.readLine();
+			String dbID = in.readLine();
+			String dbPassword = in.readLine();
+			in.close();
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection(dbURL, dbID, dbPassword);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return conn;
+	}
+	
+}
---
+++ src/reservation/util/ServerUtil.java
@@ -0,0 +1,13 @@
+package reservation.util;
+
+public class ServerUtil {
+
+	public static String host = "http://localhost:8080/JES-JSP/";
+	public static String authenticationPath = "C:\\Authentication\\";
+	public static String relativePath = "/";
+	
+	// public static String host = "http://dowellcomputer.com/JES-JSP/"; // Cafe24 호스팅 전용
+	// public static String authenticationPath = "/home/hosting_users/comzalal/Authentication/"; // Cafe24 호스팅 전용
+	// public static String relativePath = "/JES-JSP/"; // Cafe24 호스팅 전용
+	
+}
